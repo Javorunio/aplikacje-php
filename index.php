37cce04558f70ee6871eecac14c3d3852a4f90da
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 20:24:08 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $liczba = 100;
+
+    if($liczba > 99)
+    echo "Liczba jest trzycyfrowa";
+    else
+    echo "Liczba nie jest trzycyfrowa";
+    ?>
+</body>
+</html>
