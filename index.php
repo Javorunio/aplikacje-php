b8cd7792c672f292fe836cea979d86b4d8924fe4
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 19:47:57 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 5;
+    $b = 0;
+    $c = 3;
+    if($a == 0 || $b == 0 || $c ==0){
+        echo "Nie";
+    }
+    else if($a + $b <= $c || $a + $c <= $b || $b + $c <= $a){
+        echo "Nie";
+    }
+    else
+    {
+        echo "Tak";
+    }
+    ?>
+</body>
+</html>
