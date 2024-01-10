c56790bfe0ea5b201767d727af8764310f696b83
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 20:05:38 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $ocena = 1;
+
+    switch(true){
+        case $ocena == 1:
+            echo "Ocena niedostateczna";
+            break;
+        case $ocena == 2:
+            echo "Ocena dopuszczająca";
+            break;
+        case $ocena == 3:
+            echo "Ocena dostateczna";
+            break;
+        case $ocena == 4:
+            echo "Ocena dobrar";
+            break;
+        case $ocena == 5:
+            echo "Ocena bardzo dobra";
+            break;
+        case $ocena == 6:
+            echo "Ocena celująca";
+            break;
+    }
+    ?>
+</body>
+</html>
