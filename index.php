fdffc40734261e689d0832b67273f8886eee17ae
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 19:55:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $ocena = 1;
+
+    switch(true){
+        case $ocena == 1:
+            echo "Brak promocji do następnej klasy";
+            break;
+        case $ocena > 1 && $ocena < 6:
+            echo "Promocja do następnej klasy";
+            break;
+        case $ocena == 6:
+            echo "Promocja z oceną celującą";
+            break;
+    }
+    ?>
+</body>
+</html>
