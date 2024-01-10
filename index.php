3905af72895b4462dbf86faf8f009b5e97b2579f
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 20:12:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $kwota = 100;
+    $waluta = "dolar";
+
+    switch($waluta){
+        case "dolar":
+            $kwota *= 4.05;
+            echo $kwota."$";
+            break;
+        case "euro":
+            $kwota *= 4.45;
+            echo $kwota."€";
+            break;
+        case "funty":
+            $kwota *= 5.05;
+            echo $kwota."£";
+            break;
+    }
+    ?>
+</body>
+</html>
