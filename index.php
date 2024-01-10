071863f0a04744777baae0dca958ae7352477174
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 20:32:05 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $plusy = 6;
+
+    switch(true){
+        case $plusy <=9:
+            for($i = 0; $i < $plusy; $i++){
+                echo "+";
+            }
+            break;
+        case $plusy > 9:
+            echo "Wartość poza zakresem";
+            break;
+    }
+    ?>
+</body>
+</html>
