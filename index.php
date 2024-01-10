3856657051ff5a44ffb4bf1f2a937feb2abd2046
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 20:22:05 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $wiek = 21;
+
+    if($wiek > 20)
+    echo "Masz więcej niż 20 lat";
+    ?>
+</body>
+</html>
