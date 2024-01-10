8a1a825c5aa00da8557b238bb437217fe29aef53
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 19:51:52 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nr = 1;
+
+    switch($nr){
+        case 1:
+            echo "Styczeń";
+            break;
+        case 2:
+            echo "Luty";
+            break;
+        case 3:
+            echo "Marzec";
+            break;
+        case 4:
+            echo "Kwiecień";
+            break;
+        case 5:
+            echo "Maj";
+            break;
+        case 6:
+            echo "Czerwiec";
+            break;
+        case 7:
+            echo "Lipiec";
+            break;
+        case 8:
+            echo "Sierpień";
+            break;
+        case 9:
+            echo "Wrzesień";
+            break;
+        case 10:
+            echo "Październik";
+            break;
+        case 11:
+            echo "Listopad";
+            break;
+        case 12:
+            echo "Grudzień";
+            break;
+    }
+    ?>
+</body>
+</html>
