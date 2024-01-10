3b85f50e7966ec7a69358e91dc40bacbc1ad4aa3
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-10 20:42:16 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $metry = 1;
+    $jednostka = "stopa";
+
+    switch($jednostka){
+        case "cal":
+            $wynik = $metry * 100 / 2.54;
+            echo $wynik." cali";
+            break;
+        case "stopa":
+            $wynik = $metry * 100 / 30;
+            echo $wynik." stóp";
+            break;
+        case "mila morska":
+            $wynik = $metry /1852;
+            echo $wynik." mil morskich";
+            break;
+        }
+    ?>
+</body>
+</html>
