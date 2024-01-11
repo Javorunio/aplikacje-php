35aee93efc330d4ce5634e8349d24916d38b9383
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-11 14:38:19 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+        $suma = 0;
+        for ($i = 1; $i <= 10; $i++) 
+        { 
+            if ($i % 2 == 0) $suma += $i; else continue;
+            if ($i <= 10) echo "Suma: $suma <br/>";
+        }
+        ?>
+</body>
+</html>
