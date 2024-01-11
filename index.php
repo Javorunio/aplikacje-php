0c909f29290f32c834c046453c5b605724faf009
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-11 13:58:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for($i = 0; $i < 11; $i++){
+        echo "[$i] ";
+        for($j = 1; $j < 11; $j++){
+            if($i == 0){
+                echo "[$j] ";
+            }
+            else{
+                $wynik = $i * $j;
+                echo ".$wynik. ";
+            }
+        }
+        echo "<br/>";
+    }
+    ?>
+</body>
+</html>
