379644e28145c47ab52f79304ae66b0dc8d4cf7f
Javorunio <47896436+Javorunio@users.noreply.github.com>
2024-01-11 13:00:07 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "Tu nic nie ma :)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for($i = 0; $i < 2; $i++){
+        for($j = 0; $j < 10; $j++){
+            echo "| x";
+        }
+        echo "<br/>";
+    }
+    ?>
+</body>
+</html>
